c1b39c4ceee40f9f2f538b4619760728bbd842ed
Adam Christiansen <code@adamc.io>
2018-09-11 16:06:12 -0600

    Modifed the tester to match the new protocol

--- ether_tester/src/main.rs
+++ ether_tester/src/main.rs
@@ -18,9 +18,6 @@ use std::time::Duration;
 /// The number of data bytes in a test packet.
 const DATA_BYTES: usize = 256;
 
-/// The generator used to transform a seed into the next seed.
-const GENERATOR: u8 = 87;
-
 /// The parameters passed into the program.
 struct Params {
     /// The baud rate to communicate with the device.
@@ -111,6 +108,127 @@ impl Params {
     }
 }
 
+/// The parameters the describe the UDP packets to send and the data to be generated for test
+/// cases.
+struct TestParams {
+    /// The IP address of the FPGA.
+    pub src_ip: u64,
+
+    /// The MAC address of the FPGA.
+    pub src_mac: u64,
+
+    /// The port to use on the FPGA.
+    pub src_port: u16,
+
+    /// The IP address of the host machine.
+    pub dest_ip: u64,
+
+    /// The MAC address of the host machine.
+    pub dest_mac: u64,
+
+    /// The port to use on the host machine.
+    pub dest_port: u16,
+
+    /// The seed for generating data.
+    pub seed: u8,
+
+    /// The generator for deriving data from the seed.
+    pub gen: u8
+}
+
+impl TestParams {
+
+    // TODO Better value
+    /// The default IP address for the FPGA.
+    pub const DEFAULT_SRC_IP:u64 = 0;
+
+    // TODO Better value
+    /// The default MAC address for the FPGA.
+    pub const DEFAULT_SRC_MAC: u64 = 0;
+
+    // TODO Better value
+    /// The default port for the FPGA.
+    pub const DEFAULT_SRC_PORT: u16 = 0;
+
+    /// Create a new set of test parameters.
+    ///
+    /// # Arguments
+    ///
+    /// * `ip` - The IP address for the host.
+    /// * `mac` - The MAC address for the host.
+    /// * `port` - The PORT for the host.
+    ///
+    /// # Returns
+    ///
+    /// An initialized test params struct.
+    pub fn new(ip: u64, mac: u64, port: u16) -> TestParams {
+        TestParams {
+            src_ip: Self::DEFAULT_SRC_IP,
+            src_mac: Self::DEFAULT_SRC_MAC,
+            src_port: Self::DEFAULT_SRC_PORT,
+            dest_ip: ip,
+            dest_mac: mac,
+            dest_port: port,
+            seed: rand::random(),
+            gen: rand::random()
+        }
+    }
+
+    /// Convert the object to bytes that can be sent over serial.
+    ///
+    /// # Returns
+    ///
+    /// A byte array representation of the struct.
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = vec![];
+        Self::append_bytes(&mut bytes, self.src_ip, 8);
+        Self::append_bytes(&mut bytes, self.src_mac, 6);
+        Self::append_bytes(&mut bytes, self.src_port.into(), 2);
+        Self::append_bytes(&mut bytes, self.dest_ip, 8);
+        Self::append_bytes(&mut bytes, self.dest_mac, 6);
+        Self::append_bytes(&mut bytes, self.dest_port.into(), 2);
+        Self::append_bytes(&mut bytes, self.seed.into(), 1);
+        Self::append_bytes(&mut bytes, self.gen.into(), 1);
+        assert!(bytes.len() == 34);
+        return bytes
+    }
+
+    /// The expected value to receive as the payload for the test.
+    ///
+    /// # Arguments
+    ///
+    /// * `size` - The size in bytes of the payload.
+    ///
+    /// # Returns
+    ///
+    /// The expected values as an array.
+    pub fn expected(&self, size: usize) -> Vec<u8> {
+        let mut v = vec![];
+        v.push(self.seed);
+        for i in 1..size {
+            let next = v[i - 1] + self.gen;
+            v.push(next);
+        }
+        return v
+    }
+
+    /// Add values to a byte vector by deconstructing them. This makes sure that the data is
+    /// interpreted in big endian byte order.
+    ///
+    /// # Arguments
+    ///
+    /// * `vec` - The vector.
+    /// * `data` - Consists of the bytes to be added to the vector.
+    /// * `bytes` - The number of lower bytes in the value to add to the vector.
+    fn append_bytes(vec: &mut Vec<u8>, data: u64, bytes: u8) {
+        // Make sure the number if big endian
+        let v = data.to_be();
+        for i in (0..bytes).rev() {
+            vec.push(((v >> (8 * i)) & 0xFF) as u8);
+        }
+    }
+}
+
 /// Runs a single test by writing the seed to the serial port which governs the UDP packet contents
 /// that are sent over the socket.
 ///
@@ -118,17 +236,15 @@ impl Params {
 ///
 /// * `port` - An initialized serial port to communicate over.
 /// * `socket` - An initialized socket to communicate with the system.
-/// * `seed` - The seed that is used to generate the data in the packet.
-fn udp_test(port: &mut SystemPort, socket: &mut UdpSocket, seed: u8) -> Result<(), String> {
+/// * `test_params` - The test parameters to use.
+fn udp_test(port: &mut SystemPort, socket: &mut UdpSocket, test_params: &TestParams)
+-> Result<(), String> {
     // Generate the expected sequence from the seed. This is just an array starting with the seed,
     // and every element if the prebious element plus a costant.
-    let mut expected = [seed; DATA_BYTES];
-    for i in 1..expected.len() {
-        expected[i] = expected[i - 1] + GENERATOR;
-    }
+    let expected = test_params.expected(DATA_BYTES);
 
-    // Write the seed
-    if let Err(msg) = port.write(&[seed]) {
+    // Write the message to the FPGA to start the test
+    if let Err(msg) = port.write(&test_params.to_bytes()) {
         return Err(msg.to_string())
     }
 
@@ -144,7 +260,7 @@ fn udp_test(port: &mut SystemPort, socket: &mut UdpSocket, seed: u8) -> Result<(
         return Err("Error reading socket".to_string());
     }
 
-    // Chech that the read data is correct
+    // Check that the read data is correct
     if &rdata[..] != &expected[..] {
         return Err("Data received did not match the expected value".to_string());
     } else {
@@ -177,6 +293,10 @@ fn main() -> std::io::Result<()> {
     let mut socket = UdpSocket::bind(params.ip)?;
     socket.set_read_timeout(Some(Duration::new(1, 0)))?;
 
+    // Get the test parameters
+    // TODO Put the actual values in
+    let test_params = TestParams::new(0, 0, 0);
+
     // Print the title in a pretty way
     let title = format!("Reps={}", params.reps);
     println!();
@@ -185,8 +305,7 @@ fn main() -> std::io::Result<()> {
 
     let mut any_failed = false;
     for rep in 0..params.reps {
-        let seed = rand::random();
-        match udp_test(&mut port, &mut socket, seed) {
+        match udp_test(&mut port, &mut socket, &test_params) {
             Ok(_) => {
                 if !params.fail_only {
                     println!("{}", Green.paint(format!("Passed {}", rep)))
