2d724fcf6e35d94f758c641bdfcebd6f28acfba1
Adam Christiansen <hello@adamc.io>
2018-09-17 15:52:24 -0600

    Tester now creates a socket

--- ether_tester/src/main.rs
+++ ether_tester/src/main.rs
@@ -7,6 +7,7 @@ extern crate serial;
 use serial::*;
 use std::fmt::Display;
 use std::io::Write;
+use std::net::UdpSocket;
 use std::result::Result;
 
 mod params;
@@ -23,29 +24,47 @@ fn main() {
     // Get the command line arguments
     let params = match Params::get() {
         Ok(p) => p,
-        Err(msg) => fatal("bad command line argument", msg)
+        Err(msg) => fatal("Bad command line argument", msg)
     };
 
+    // Print the test parameters
+    println!("Parameters");
+    println!("----------");
+    println!("{} {}", Green.paint("Source         "), Green.paint("(Test Device)"));
+    println!("{} {}", Green.paint("  IP           "), params.src_ip_string());
+    println!("{} {}", Green.paint("  Port         "), params.src_port);
+    println!("{} {}", Green.paint("  Mac          "), params.src_mac_string());
+    println!("{} {}", Green.paint("Destination    "), Green.paint("(Host Device)"));
+    println!("{} {}", Green.paint("  IP           "), params.dest_ip_string());
+    println!("{} {}", Green.paint("  Port         "), params.dest_port);
+    println!("{} {}", Green.paint("  Mac          "), params.dest_mac_string());
+    println!("{} {}", Green.paint("Serial Port    "), params.serial_port);
+    println!("{} {}", Green.paint("Serial Baudrate"), params.serial_baud.speed());
+    println!();
+
     // Open a new port
-    // TODO Verify port opened and configured successfully
-    let mut port = serial::open(&params.serial_port).expect("Could not open port");
-    let _ = port.reconfigure(&|settings| {
+    let mut port = match serial::open(&params.serial_port) {
+        Ok(p) => p,
+        Err(err) => fatal("Could not open serial port", err.to_string())
+    };
+    match port.reconfigure(&|settings| {
         settings.set_baud_rate(params.serial_baud)?;
         settings.set_char_size(Bits8);
         settings.set_parity(ParityNone);
         settings.set_stop_bits(Stop1);
         settings.set_flow_control(FlowNone);
         Ok(())
-    });
+    }) {
+        Ok(_) => {},
+        Err(err) => fatal("Could not change serial settings", err.to_string())
+    }
 
-    // Print the test information
-    println!("Parameters");
-    println!("----------");
-    println!("{} {}", Green.paint("Source         "), params.src_info());
-    println!("{} {}", Green.paint("Destination    "), params.dest_info());
-    println!("{} {}", Green.paint("Serial Port    "), params.serial_port);
-    println!("{} {}", Green.paint("Serial Baudrate"), params.serial_baud.speed());
-    println!();
+    // Bind a socket to the test system
+    let socket_addr = format!("{}:{}", params.src_ip_string(), params.src_port);
+    let socket = match UdpSocket::bind(socket_addr) {
+        Ok(s) => s,
+        Err(err) => fatal("Could not open socket", err.to_string())
+    };
 
     println!("Results");
     println!("-------");
@@ -58,8 +77,25 @@ fn main() {
             .map_err(|err| err.to_string())
             // Read the incoming Ethernet data and compare it to the expected data
             .and_then(|_| {
-                // TODO
-                Ok(())
+                // Read the packet
+                let mut buf = vec![0; params.bytes];
+                match socket.recv_from(&mut buf) {
+                    Ok((size, _socket_addr)) => {
+                        let expected = test_case.expected();
+                        if size != expected.len() {
+                            return Err(format!(
+                                "Received datagram is {} bytes, expected {} bytes", size,
+                                expected.len()))
+                        }
+                        // The buffer and the expected data are the same size
+                        return if buf == expected {
+                            Ok(())
+                        } else {
+                            Err("The datagram did not match the expected value".to_string())
+                        }
+                    }
+                    Err(err) => Err(format!("Could not read socket: {}", err.to_string()))
+                }
             });
         // Print output
         match result {
